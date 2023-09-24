7fa5b4bf3f010393184495a5f4d698e2911b6d8c
Dmitry Lepyanske <lepyanske@yandex.ru>
2023-09-24 22:55:14 +0300

    функции добавления и вычитания

---
+++ main.py
@@ -0,0 +1,7 @@
+from funcs import add, subtract
+
+result_add = add(5, 3)
+result_subtract = subtract(10, 4)
+
+print(f"Addition result: {result_add}")
+print(f"Subtraction result: {result_subtract}")
